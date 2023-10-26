133ef9086c691b23e5c98d8f11d8177abdf4bcb1
murzakanovim <murzakanovislam21@gmail.com>
2023-10-26 03:56:09 +0300

    LAB  DONE

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <cmath>
+#include <chrono>
 #include <SOIL/SOIL.h>
 
 const float sphereRadius = 1.0f;
@@ -20,7 +21,7 @@ float debugz = 0.0f;
 
 const float cameraSpeed = 2.0f;
 
-int move = 900;
+int move = 1000;
 float gscale = 0.6;
 
 void specialKeys(int key, int, int);
@@ -28,7 +29,7 @@ void specialKeys(int key, int, int);
 void drawOctahedron(GLfloat xcord, GLfloat ycord, GLfloat zcord,
                     GLfloat xrotl, GLfloat yrotl, GLfloat zrotl);
 
-void drawSphere();
+void drawSphere(GLfloat x, GLfloat y, GLfloat z, GLdouble radius);
 
 void drawCube(GLfloat xcord, GLfloat ycord, GLfloat zcord, GLdouble scale);
 
@@ -84,10 +85,10 @@ void keyboard(unsigned char key, int, int)
         debugx = debugy = debugz = 0;
         break;
     case '.':
-        move += 1;
+        move += 10;
         break;
     case ',':
-        move -= 1;
+        move -= 10;
         break;
     case 'z':
         gscale += 0.1;
@@ -102,6 +103,87 @@ void keyboard(unsigned char key, int, int)
     glutPostRedisplay();
 }
 
+void test()
+{
+    auto move_val = move % 1400;
+    if (move_val >= 0 && move_val <= 100)
+    {
+        drawOctahedron(0, 0 + move_val * 0.14 / 100, -0.8 + move_val * 0.05 / 100, 36 - move_val * 36 / 100, -45, 0);
+    }
+    if (move_val > 100 && move_val < 200)
+    {
+        drawOctahedron(0, 0.14 + (move_val - 100) * 0.14 / 100, -0.75 - (move_val - 100) * 0.05 / 100,
+                       0 - (move_val - 100) * 35.25 / 100, -45, 0);
+    }
+    if (move_val >= 200 && move_val < 300)
+    {
+        drawOctahedron(0 + (move_val - 200) * 0.15 / 100, 0.28 + (move_val - 200) * 0.08 / 100, -0.80 + (move_val - 200) * 0.05 / 100,
+                       -35.25 - (move_val - 200) * 6.4 / 100, -45 + (move_val - 200) * 28.1 / 100, 0 + (move_val - 200) * 18.3 / 100);
+    }
+    if (move_val >= 300 && move_val < 400)
+    {
+
+        drawOctahedron(0.15 + (move_val - 300) * 0.11 / 100, 0.36 + (move_val - 300) * 0.02 / 100, -0.75 - (move_val - 300) * 0.05 / 100,
+                       -41.65 - (move_val - 300) * 12.8 / 100, -16.9 + (move_val - 300) * 17.1 / 100, 18.3 + (move_val - 300) * 26.1 / 100);
+    }
+
+    if (move_val >= 400 && move_val < 500)
+    {
+        drawOctahedron(0.26, 0.38, -0.8 + (move_val - 400) * 0.05 / 100,
+                       -54.45 - (move_val - 400) * 36.3 / 100, 0.2 + (move_val - 400) * 0.2 / 100, 44.4 + (move_val - 400) * 0.4 / 100);
+    }
+
+    if (move_val >= 500 && move_val < 600)
+    {
+        drawOctahedron(0.26, 0.38 + (move_val - 500) * 0.15 / 100, -0.75 - (move_val - 500) * 0.05 / 100,
+                       -90.75 - (move_val - 500) * 34.4 / 100, 0.4 - (move_val - 500) * 0.4 / 100, 44.8 + (move_val - 500) * 0.2 / 100);
+    }
+
+    if (move_val >= 600 && move_val < 700)
+    {
+        drawOctahedron(0.26 - (move_val - 600) * 0.18 / 100, 0.53 + (move_val - 600) * 0.12 / 100, -0.8 + (move_val - 600) * 0.05 / 100,
+                       -125.15 - (move_val - 600) * 13.4 / 100, 0 + (move_val - 600) * 19.1 / 100, 45 - (move_val - 600) * 28.6 / 100);
+    }
+
+    if (move_val >= 700 && move_val < 800)
+    {
+        drawOctahedron(0.08 - (move_val - 700) * .12 / 100, 0.65 + (move_val - 700) * 0.04 / 100, -0.75 - (move_val - 700) * .05 / 100,
+                       -138.55 - (move_val - 700) * 7.7 / 100, 19.1 + (move_val - 700) * 26.9 / 100, 16.4 - (move_val - 700) * 15.2 / 100);
+    }
+
+    if (move_val >= 800 && move_val < 900)
+    {
+        drawOctahedron(-0.04, 0.69 + (move_val - 800) * 0.13 / 100, -0.8 + (move_val - 800) * 0.04 / 100,
+                       -146.25 - (move_val - 800) * 32.4 / 100, 46 - (move_val - 800) * 1 / 100, 1.2 - (move_val - 800) * 2.8 / 100);
+    }
+    
+    if (move_val >= 900 && move_val < 1000)
+    {
+        drawOctahedron(-0.04, 0.82 + (move_val - 900) * 0.15 / 100, -0.76 - (move_val - 900) * 0.04 / 100,
+                       -178.65 - (move_val - 900) * 36.4 / 100, 45, -1.6 + (move_val - 900) * 1.7 / 100);
+    }
+    if (move_val >= 1000 && move_val <= 1100)
+    {
+        drawOctahedron(-0.04 - (move_val - 1000) * 0.13 / 100, 0.97 + (move_val - 1000) * 0.06 / 100, -0.8 + (move_val - 1000) * 0.05 / 100,
+                       -215.05 - (move_val - 1000) * 55.5 / 100, 45 + (move_val - 1000) * 15.8 / 100, 0.1 + (move_val - 1000) * 45.5 / 100);
+    }
+    if (move_val > 1100 && move_val <= 1200)
+    {
+        drawOctahedron(-0.17 - ( move_val - 1100 ) * .13 / 100, 1.03 + ( move_val - 1100 ) * .07 / 100, -0.75 - ( move_val - 1100 ) * 0.05 / 100,
+                       -270.55 - ( move_val - 1100) * 53.9 / 100, 60.8 - ( move_val - 1100 ) * 15.8 / 100, 45.6 + ( move_val - 1100 ) * 44.2 / 100);
+    }
+    if (move_val > 1200 && move_val <= 1300)
+    {
+        drawOctahedron(-0.3 - ( move_val - 1200 ) * 0.13 / 100,1.1 - ( move_val - 1200 ) * 0.07 / 100, -0.80 + ( move_val - 1200 ) * 0.05 / 100,
+                       -324.45 + ( move_val - 1200 ) * 6.1 / 100, 45 - ( move_val - 1200 ) * 22.0 / 100, 89.8 + ( move_val - 1200 ) * 23.8 / 100);
+    }
+    if (move_val > 1300 && move_val < 1400)
+    {
+        drawOctahedron(-0.43- ( move_val - 1300 ) * 0.12 / 100, 1.03- ( move_val - 1300 ) * 0.07 / 100, -0.75- ( move_val - 1300 ) * 0.05 / 100,
+                       -318.35 + ( move_val - 1300 ) * 13.0 / 100, 23 - ( move_val - 1300 ) * 23.1 / 100, 113.6 + ( move_val - 1300 ) * 22.1 / 100);
+    }
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -110,66 +192,31 @@ void display()
     glRotatef(cameraY, 0.0, 1.0, 0.0);
     glRotatef(cameraZ, 0.0, 0.0, 1.0);
     glScalef(gscale, gscale, gscale);
-
-    if (move >= 0 && move <= 100)
-    {
-        drawOctahedron(0, 0 + move * 0.14 / 100, -0.8 + move * 0.05 / 100, 36 - move * 36 / 100, -45, 0);
-    }
-    if (move > 100 && move < 200)
-    {
-        drawOctahedron(0, 0.14 + (move - 100) * 0.14 / 100, -0.75 - (move - 100) * 0.05 / 100,
-                       0 - (move - 100) * 35.25 / 100, -45, 0);
-    }
-    if (move >= 200 && move < 300)
-    {
-        drawOctahedron(0 + (move - 200) * 0.15 / 100, 0.28 + (move - 200) * 0.08 / 100, -0.80 + (move - 200) * 0.05 / 100,
-                       -35.25 - (move - 200) * 6.4 / 100, -45 + (move - 200) * 28.1 / 100, 0 + (move - 200) * 18.3 / 100);
-    }
-    if (move >= 300 && move < 400)
-    {
-
-        drawOctahedron(0.15 + (move - 300) * 0.11 / 100, 0.36 + (move - 300) * 0.02 / 100, -0.75 - (move - 300) * 0.05 / 100,
-                       -41.65 - (move - 300) * 12.8 / 100, -16.9 + (move - 300) * 17.1 / 100, 18.3 + (move - 300) * 26.1 / 100);
-    }
-
-    if (move >= 400 && move < 500)
-    {
-        drawOctahedron(0.26, 0.38, -0.8 + (move - 400) * 0.05 / 100,
-                       -54.45 - (move - 400) * 36.3 / 100, 0.2 + (move - 400) * 0.2 / 100, 44.4 + (move - 400) * 0.4 / 100);
-    }
-
-    if (move >= 500 && move < 600)
-    {
-        drawOctahedron(0.26, 0.38 + (move - 500) * 0.15 / 100, -0.75 - (move - 500) * 0.05 / 100,
-                       -90.75 - (move - 500) * 34.4 / 100, 0.4 - (move - 500) * 0.4 / 100, 44.8 + (move - 500) * 0.2 / 100);
-    }
-
-    if (move >= 600 && move < 700)
-    {
-        drawOctahedron(0.26 - (move - 600) * 0.18 / 100, 0.53 + (move - 600) * 0.12 / 100, -0.8 + (move - 600) * 0.05 / 100,
-                       -125.15 - (move - 600) * 13.4 / 100, 0 + (move - 600) * 19.1 / 100, 45 - (move - 600) * 28.6 / 100);
-    }
-
-    if (move >= 700 && move < 800)
-    {
-        drawOctahedron(0.08 - (move - 700) * .12 / 100, 0.65 + (move - 700) * 0.04 / 100, -0.75 - (move - 700) * .05 / 100,
-                       -138.55 - (move - 700) * 7.7 / 100, 19.1 + (move - 700) * 26.9 / 100, 16.4 - (move - 700) * 15.2 / 100);
-    }
-
-    if (move >= 800 && move < 900)
-    {
-        drawOctahedron(-0.04, 0.69 + (move - 800) * 0.13 / 100, -0.8 + (move - 800) * 0.04 / 100,
-                       -146.25 - (move - 800) * 32.4 / 100, 46 - (move - 800) * 1 / 100, 1.2 - (move - 800) * 2.8 / 100);
-    }
     
-    if (move >= 900 && move < 1000)
+    if ( move >= 2800 )
     {
-        drawOctahedron(-0.04, 0.82 + (move - 900) * 0.15 / 100, -0.76 - (move - 900) * 0.04 / 100,
-                       -178.65 - (move - 900) * 36.4 / 100, 45, -1.6 + (move - 900) * 1.7 / 100);
+        move = 0;
     }
+
+    if ( move >= 1400 )
+    {
+        glPushMatrix();
+
+        glRotatef(180, 1.0, 0.0, 0.0);
+        glRotatef(180, 0.0, 1.0, 0.0);
+        glTranslatef(0.43+0.12, -1.03+0.07, 0);
+
+        test();
+        glPopMatrix();
+    }
+    else
+    {
+        test();
+    }
+
     drawCube(0, 0, -2.5, 3);
 
-    // drawSphere();
+    drawSphere(-0.2,0.4,-0.9,0.1);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -203,25 +250,27 @@ void init()
 
     glEnable(GL_NORMALIZE);
     glEnable(GL_DEPTH_TEST);
+
 }
 
 void drawCube(GLfloat xcord, GLfloat ycord, GLfloat zcord, GLdouble scale)
 {
     glPushMatrix();
     glTranslatef(xcord, ycord, zcord);
+
     glutSolidCube(scale);
     glPopMatrix();
 }
 
-void drawSphere()
+void drawSphere(GLfloat x, GLfloat y, GLfloat z, GLdouble radius)
 {
     glPushMatrix();
-    glTranslatef(0, 0, sphereRadius);
+    glTranslatef(x, y, z);
     glColor3f(1.0f, .0f, 0.0f);
     const int numSlices = 50;
     const int numStacks = 50;
 
-    glutWireSphere(sphereRadius, numSlices, numStacks);
+    glutSolidSphere(radius, numSlices, numStacks);
     glPopMatrix();
 }
 
